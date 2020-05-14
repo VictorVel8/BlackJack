721f04359b6911288d16e17cdbd92bfcf741e442
VictorVel8 <victorvel801@gmail.com>
2020-05-14 23:34:40 +0300

    First commit for blaghino

---
+++ BlackJack/BlackJack/Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace BlackJack
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.split = new System.Windows.Forms.Button();
+            this.hit = new System.Windows.Forms.Button();
+            this.stand = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dealer = new System.Windows.Forms.Label();
+            this.player = new System.Windows.Forms.Label();
+            this.sdealer = new System.Windows.Forms.Label();
+            this.splayer = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // split
+            // 
+            this.split.Location = new System.Drawing.Point(513, 448);
+            this.split.Name = "split";
+            this.split.Size = new System.Drawing.Size(117, 50);
+            this.split.TabIndex = 0;
+            this.split.Text = "Imparte Cartile";
+            this.split.UseVisualStyleBackColor = true;
+            this.split.Click += new System.EventHandler(this.split_Click);
+            // 
+            // hit
+            // 
+            this.hit.Location = new System.Drawing.Point(257, 448);
+            this.hit.Name = "hit";
+            this.hit.Size = new System.Drawing.Size(102, 50);
+            this.hit.TabIndex = 1;
+            this.hit.Text = "Hit";
+            this.hit.UseVisualStyleBackColor = true;
+            this.hit.Visible = false;
+            this.hit.Click += new System.EventHandler(this.hit_Click);
+            // 
+            // stand
+            // 
+            this.stand.Location = new System.Drawing.Point(765, 448);
+            this.stand.Name = "stand";
+            this.stand.Size = new System.Drawing.Size(116, 50);
+            this.stand.TabIndex = 2;
+            this.stand.Text = "Stand";
+            this.stand.UseVisualStyleBackColor = true;
+            this.stand.Visible = false;
+            this.stand.Click += new System.EventHandler(this.stand_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(470, 28);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(187, 33);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Carti dealer:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(470, 249);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(186, 33);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Carti player:";
+            // 
+            // dealer
+            // 
+            this.dealer.AutoSize = true;
+            this.dealer.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dealer.Location = new System.Drawing.Point(527, 94);
+            this.dealer.Name = "dealer";
+            this.dealer.Size = new System.Drawing.Size(0, 29);
+            this.dealer.TabIndex = 5;
+            // 
+            // player
+            // 
+            this.player.AutoSize = true;
+            this.player.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.player.Location = new System.Drawing.Point(508, 295);
+            this.player.Name = "player";
+            this.player.Size = new System.Drawing.Size(0, 29);
+            this.player.TabIndex = 6;
+            // 
+            // sdealer
+            // 
+            this.sdealer.AutoSize = true;
+            this.sdealer.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.sdealer.Location = new System.Drawing.Point(945, 44);
+            this.sdealer.Name = "sdealer";
+            this.sdealer.Size = new System.Drawing.Size(0, 24);
+            this.sdealer.TabIndex = 7;
+            // 
+            // splayer
+            // 
+            this.splayer.AutoSize = true;
+            this.splayer.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.splayer.Location = new System.Drawing.Point(945, 268);
+            this.splayer.Name = "splayer";
+            this.splayer.Size = new System.Drawing.Size(0, 24);
+            this.splayer.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(490, 157);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(115, 39);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "label3";
+            this.label1.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1191, 578);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.splayer);
+            this.Controls.Add(this.sdealer);
+            this.Controls.Add(this.player);
+            this.Controls.Add(this.dealer);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.stand);
+            this.Controls.Add(this.hit);
+            this.Controls.Add(this.split);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button split;
+        private System.Windows.Forms.Button hit;
+        private System.Windows.Forms.Button stand;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label dealer;
+        private System.Windows.Forms.Label player;
+        private System.Windows.Forms.Label sdealer;
+        private System.Windows.Forms.Label splayer;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ BlackJack/BlackJack/Form1.cs
@@ -0,0 +1,309 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BlackJack
+{
+    public partial class Form1 : Form
+    {
+        int[] fr;
+        int sEu, sDealer;
+        int nrAeu, nrAdealer;
+        List<int> cartiEu;
+        List<int> cartiDealer;
+        Random rnd;
+        string strCartiEu="", strCartiDealer="";
+        bool ok = false;
+        bool ok2 = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+            fr = new int[20];
+            cartiDealer = new List<int>();
+            cartiEu = new List<int>();
+            rnd = new Random();
+            nrAdealer = 0;
+            nrAeu = 0;
+            sEu = sDealer = 0;
+        }       
+
+        int createRandomCard()
+        {
+            
+            int valoare = rnd.Next(2, 15);
+            return valoare;
+        }
+
+        int verificareSiReturnare(int tura)
+        {
+            int val = createRandomCard();
+            while(fr[val]>=4 || (tura==1 &&nrAeu!=0 && val==11) || (tura==2 && nrAdealer!=0 && val==11))
+            {
+                val = createRandomCard();
+            }
+            fr[val]++;
+
+            if (tura==2)
+            {
+                if (sDealer == 0)
+                    val = 11;
+                else
+                    val = 5;
+            }
+            return val;
+        }
+
+        void adaugareCarteDealer(int carte)
+        {
+            cartiDealer.Add(carte);
+        }
+
+        void adaugareCarteEu(int carte)
+        {
+            cartiEu.Add(carte);
+        }
+
+        string valToStr(int valoare)
+        {
+            if (valoare > 0 && valoare <= 10)
+                return valoare.ToString();
+            else if (valoare == 11)
+                return "A";
+            else if (valoare == 12)
+                return "J";
+            else if (valoare == 13)
+                return "Q";
+            else if (valoare == 14)
+                return "K";
+            return "0";
+        }
+
+        private void hit_Click(object sender, EventArgs e)
+        {
+            int valoare;
+
+            valoare = verificareSiReturnare(1);
+            strCartiEu += valToStr(valoare) + " ";
+            player.Text = strCartiEu;
+            Application.DoEvents();
+            adaugareCarteEu(valoare);
+            if (valoare == 11)
+                nrAeu++;
+            if (valoare >= 10 && valoare < 15)
+                sEu += 10;
+            else
+                sEu += valoare;
+            afisareSumPlayer();
+            if (nrAeu!=0)
+            {
+                if(sEu-9>21)
+                {
+                    stand.Visible = false;
+                    hit.Visible = false;
+                    split.Visible = true;
+                    label1.Visible = true;
+                    label1.Text = "Dealer Won";
+                }
+            }
+            else
+            {
+                if (sEu > 21)
+                {
+                    stand.Visible = false;
+                    hit.Visible = false;
+                    split.Visible = true;
+                    label1.Visible = true;
+                    label1.Text = "Dealer Won";
+                }
+            }
+        }
+
+        private void verdict()
+        {
+            label1.Visible = true;
+            if (sDealer > 21)
+            {
+                label1.Text = "You Won";
+            }
+            else
+            {
+                if (nrAeu!=0)
+                {
+                    if (sEu + 1 <= 21)
+                        sEu++;
+                    else
+                        sEu -= 9;
+                }
+                if (nrAdealer!= 0)
+                {
+                    if (sDealer + 1 <= 21)
+                        sDealer++;
+                    else
+                        sDealer-= 9;
+                }
+                if (21 - sDealer < 21 - sEu)
+                    label1.Text = "Dealer Won";
+                else if (21 - sDealer > 21 - sEu)
+                    label1.Text = "You Won";
+                else
+                    label1.Text = "Even";
+            }
+
+            hit.Visible = false;
+            stand.Visible = false;
+            split.Visible = true;
+        }
+
+        private void stand_Click(object sender, EventArgs e)
+        {
+            int valoare;
+            while(sDealer<16)
+            {
+                if (nrAdealer > 0 && sDealer + 1 >= 16)
+                    break;
+                valoare = verificareSiReturnare(2);
+                adaugareCarteDealer(valoare);
+                strCartiDealer += valToStr(valoare) + " ";
+                dealer.Text = strCartiDealer;
+                if (valoare == 11)
+                    nrAdealer++;
+                if (valoare >= 10 && valoare < 15)
+                    sDealer += 10;
+                else
+                    sDealer += valoare;
+                if (nrAdealer!=0)
+                {
+                    if (sDealer+1>21 && ok==false)
+                    {
+                        sDealer -= 9;
+                        ok = true;
+                    }
+                }
+                
+                afisareSumDealer();
+                Application.DoEvents();
+                System.Threading.Thread.Sleep(1000);
+            }
+
+            verdict();
+
+            
+
+        }
+        void afisareSumPlayer()
+        {
+            if (nrAeu!=0)
+            {
+                if (sEu+1<=21)
+                {
+                    splayer.Text = (sEu+1).ToString() + "/" + (sEu - 9).ToString();
+                }
+                else
+                {
+                    splayer.Text = (sEu - 9).ToString();
+                }
+            }
+            else
+            {
+                splayer.Text = sEu.ToString();
+            }
+        }
+
+        void afisareSumDealer()
+        {
+            if (nrAdealer != 0 && ok == false)
+            {
+                if (sDealer + 1 <= 21)
+                {
+                    sdealer.Text = (sDealer + 1).ToString() + "/" + (sDealer - 9).ToString();
+                }
+                else
+                {
+                    sdealer.Text = (sDealer - 10).ToString();
+                }
+            }
+            else
+            {
+                sdealer.Text = sDealer.ToString();
+            }
+        }
+
+        private void split_Click(object sender, EventArgs e)
+        {
+            hit.Visible = true;
+            stand.Visible = true;
+            label1.Visible = false;
+            Application.DoEvents();
+            dealer.Text = "";
+            player.Text = "";
+            splayer.Text = "";
+            sdealer.Text = "";
+            strCartiDealer = "";
+            strCartiEu = "";
+            sEu = 0;
+            sDealer = 0;
+            nrAdealer = 0;
+            nrAeu = 0;
+            ok = false;
+            ok2 = false;
+
+            for (int i = 2; i <= 15; ++i)
+                fr[i] = 0;
+            cartiEu.Clear();
+            cartiDealer.Clear();
+            int valoare;
+
+            valoare = verificareSiReturnare(1);
+            strCartiEu += valToStr(valoare) + " ";
+            player.Text = strCartiEu;
+            adaugareCarteEu(valoare);
+            if (valoare == 11)
+                nrAeu++;
+            if (valoare >= 10 && valoare < 15)
+                sEu += 10;
+            else
+                sEu += valoare;
+            Application.DoEvents();
+            System.Threading.Thread.Sleep(500);
+
+            valoare = verificareSiReturnare(2);
+            adaugareCarteDealer(valoare);
+            strCartiDealer += valToStr(valoare) + " ";
+            dealer.Text = strCartiDealer;
+            if (valoare == 11)
+                nrAdealer++;
+            if (valoare >= 10 && valoare < 15)
+                sDealer += 10;
+            else
+                sDealer += valoare;
+            Application.DoEvents();
+            System.Threading.Thread.Sleep(500);
+
+            valoare = verificareSiReturnare(1);
+            adaugareCarteEu(valoare);
+            strCartiEu += valToStr(valoare) + " ";
+            player.Text = strCartiEu;
+            if (valoare == 11)
+                nrAeu++;
+            if (valoare >= 10 && valoare < 15)
+                sEu += 10;
+            else
+                sEu += valoare;
+
+
+            afisareSumPlayer();
+            afisareSumDealer();
+
+
+            split.Visible = false;
+
+        }
+    }
+}
